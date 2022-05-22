cf233cbc862a1b7bc83826d8018bca308652238a
Andrew Anca <andrewanca1998@hotmail.com>
2022-05-22 15:54:01 -0700

    final updates

--- public/login.js
+++ public/login.js
@@ -1,27 +1,3 @@
-// function to handle the register button being clicked
-function register() {
-    // set data = to value of fields typed by user
-    let data = {
-        username: $("#username").val(),
-        password: $("#password").val()
-    };
-
-    // fetch promise to response objects
-    fetch('/register', {
-        // method type
-        method: 'POST',
-        body: JSON.stringify(data),
-        headers: {
-            'Content-type': 'application/json'
-        }
-        // .then to handle asynchronous call
-    }).then(response => response.json()).then((data) => {
-        // if successfull display message and data
-        console.log(data.success);
-        console.log(data.message);
-    });
-};
-
 // function to handle the login button being clicked
 function login() {
     // set data = to value of fields typed by user
