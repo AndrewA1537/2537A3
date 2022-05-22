9fde5cb4135537d0589ed9e8fa7157a2ba077f36
Andrew Anca <andrewanca1998@hotmail.com>
2022-05-22 01:10:00 -0700

    cleaning up

--- public/login.js
+++ public/login.js
@@ -1,40 +1,52 @@
+// function to handle the register button being clicked
 function register() {
+    // set data = to value of fields typed by user
     let data = {
         username: $("#username").val(),
         password: $("#password").val()
     }
 
+    // fetch promise to response objects
     fetch('/register', {
+        // method type
         method: 'POST',
         body: JSON.stringify(data),
         headers: {
             'Content-type': 'application/json'
         }
+        // .then to handle asynchronous call
     }).then(response => response.json()).then((data) => {
-        // On success, redirect (refresh) to login
+        // if successfull display message and data
         console.log(data.success);
         console.log(data.message);
     })
 }
 
+// function to handle the login button being clicked
 function login() {
+    // set data = to value of fields typed by user
     let data = {
         username: $("#username").val(),
         password: $("#password").val()
     }
 
+    // fetch promise to response objects
     fetch('/login', {
+        // method type
         method: 'POST',
         body: JSON.stringify(data),
         headers: {
             'Content-type': 'application/json'
         }
+        // .then to handle asynchronous call
     }).then(response => response.json()).then((data) => {
-        // If authentication was successful, redirect to user profile, else display an error message
+        // if successful auth
         if (data.success) {
+            // redirect to user profile,
             window.location.href = '/profile'
         } else {
+            // or display an error message
             $("#error-text").text("Invalid login: Try again")
-        }
-    })
-}
+        };
+    });
+};
